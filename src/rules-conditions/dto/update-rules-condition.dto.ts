0042011496dd2f5c2df5ae46504bc5d168596148
JhodaDev <rojasjhonatan324@gmail.com>
2024-09-15 18:24:05 -0500

    feat: remove RulesConditions module, controller, service, and DTOs to streamline codebase and reduce complexity

--- src/rules-conditions/dto/update-rules-condition.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateRulesConditionDto } from './create-rules-condition.dto';
-
-export class UpdateRulesConditionDto extends PartialType(CreateRulesConditionDto) {}
