03dbfc2ee308cac75b2306075b0851ee244e4fe1
JhodaDev <rojasjhonatan324@gmail.com>
2024-09-15 19:21:37 -0500

    feat: add ApiTags decorator to ConditionGroupsController for improved Swagger documentation and API organization

--- src/condition_groups/condition_groups.controller.ts
+++ src/condition_groups/condition_groups.controller.ts
@@ -1,7 +1,9 @@
 import { Controller, Post, Body } from '@nestjs/common'
 import { ConditionGroupsService } from './condition_groups.service'
 import { CreateConditionGroupDto } from './dto/create-condition_group.dto'
+import { ApiTags } from '@nestjs/swagger'
 
+@ApiTags('ConditionGroups')
 @Controller('condition-groups')
 export class ConditionGroupsController {
   constructor(private readonly conditionGroupsService: ConditionGroupsService) {}
