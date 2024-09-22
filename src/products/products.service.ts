e98ffa92057c7262fca3db06f49f626e09bd779b
JhodaDev <rojasjhonatan324@gmail.com>
2024-09-22 17:44:25 -0500

    feat: update product creation logic to save new products in the database via the repository instead of a placeholder message

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@nestjs/common'
 import { CreateProductDto } from './dto/create-product.dto'
-import { UpdateProductDto } from './dto/update-product.dto'
 import { InjectRepository } from '@nestjs/typeorm'
 import { Product } from './entities/product.entity'
 import { Repository } from 'typeorm'
@@ -10,6 +9,6 @@ export class ProductsService {
   constructor(@InjectRepository(Product) private readonly productRepository: Repository<Product>) {}
 
   create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product'
+    return this.productRepository.save(createProductDto)
   }
 }
