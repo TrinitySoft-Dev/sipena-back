c972e9014784b9646cbd2e8123a3f554c7c6a089
JhodaDev <rojasjhonatan324@gmail.com>
2024-10-08 21:16:44 -0500

    feat: add ApiTags annotation to PasswordHashController for improved Swagger documentation and enhanced API organization

--- src/password_hash/password_hash.controller.ts
+++ src/password_hash/password_hash.controller.ts
@@ -1,7 +1,9 @@
-import { Controller, Post, Body, Delete } from '@nestjs/common'
+import { Controller, Post, Body, Delete, UseGuards } from '@nestjs/common'
 import { PasswordHashService } from './password_hash.service'
 import { CreatePasswordHashDto } from './dto/create-password_hash.dto'
+import { ApiTags } from '@nestjs/swagger'
 
+@ApiTags('Password Hash')
 @Controller('password-hash')
 export class PasswordHashController {
   constructor(private readonly passwordHashService: PasswordHashService) {}
