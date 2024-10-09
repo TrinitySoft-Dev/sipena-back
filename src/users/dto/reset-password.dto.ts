93d618d0d77054fd13fba387acd6281366fd0031
JhodaDev <rojasjhonatan324@gmail.com>
2024-10-08 21:46:51 -0500

    feat: enhance ResetPasswordDto with ApiProperty decorators for improved Swagger docs and better clarity on user input requirements

--- src/users/dto/reset-password.dto.ts
+++ src/users/dto/reset-password.dto.ts
@@ -1,9 +1,18 @@
+import { ApiProperty } from '@nestjs/swagger'
 import { IsString, MinLength } from 'class-validator'
 
 export class ResetPasswordDto {
+  @ApiProperty({
+    description: 'Token of the user',
+    example: 'asidohnwqekl123',
+  })
   @IsString()
   token: string
 
+  @ApiProperty({
+    description: 'New password of the user',
+    example: '123456',
+  })
   @IsString()
   @MinLength(6)
   newPassword: string
